64cac75d0f4da8208f2a095ace8d28091d270e67
ptrcnull <git@ptrcnull.me>
2024-04-11 14:23:51 +0200

    add more colours

--- ui/colors.go
+++ ui/colors.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"hash/fnv"
 	"math"
+	"strings"
 
 	"git.sr.ht/~rockorager/vaxis"
 )
@@ -101,6 +102,34 @@ func (ui *UI) IdentColor(scheme ColorScheme, ident string, self bool) vaxis.Colo
 	default:
 		panic("invalid color scheme setting")
 	}
+	baseName := strings.ToLower(ident)
+	var angleBase uint64 = 0
+	angleBase += uint64(CapLetter(baseName[0])) * 28
+	if len(baseName) > 1 {
+		angleBase += uint64(CapLetter(baseName[1]))
+	}
+	// full spectrum
+	var maxValues float64 = 27 * 28
+	// make it rotate thrice
+	maxValues /= 3
+
+	_, angle := math.Modf(float64(angleBase) / maxValues)
+	// 360 no scope
+	hue := angle * 360
+
+	return vaxis.RGBColor(HSVToRGB(hue, 1, 1))
+}
+
+// returns a value between 0 and 27 for a given character
+func CapLetter(value byte) byte {
+	if value < 'a' {
+		value = ('a' - 1)
+	}
+	if value > 'z' {
+		value = ('z' + 1)
+	}
+	value -= ('a' - 1)
+	return value
 }
 
 func (ui *UI) IdentString(scheme ColorScheme, ident string, self bool) StyledString {
@@ -109,4 +138,36 @@ func (ui *UI) IdentString(scheme ColorScheme, ident string, self bool) StyledStr
 		Foreground: color,
 	}
 	return Styled(ident, style)
+}
+
+// HSVToRGB converts an HSV triple to an RGB triple.
+func HSVToRGB(h, s, v float64) (r, g, b uint8) {
+	if h < 0 || h >= 360 ||
+		s < 0 || s > 1 ||
+		v < 0 || v > 1 {
+		return 0, 0, 0
+	}
+	// When 0 ≤ h < 360, 0 ≤ s ≤ 1 and 0 ≤ v ≤ 1:
+	C := v * s
+	X := C * (1 - math.Abs(math.Mod(h/60, 2)-1))
+	m := v - C
+	var Rnot, Gnot, Bnot float64
+	switch {
+	case 0 <= h && h < 60:
+		Rnot, Gnot, Bnot = C, X, 0
+	case 60 <= h && h < 120:
+		Rnot, Gnot, Bnot = X, C, 0
+	case 120 <= h && h < 180:
+		Rnot, Gnot, Bnot = 0, C, X
+	case 180 <= h && h < 240:
+		Rnot, Gnot, Bnot = 0, X, C
+	case 240 <= h && h < 300:
+		Rnot, Gnot, Bnot = X, 0, C
+	case 300 <= h && h < 360:
+		Rnot, Gnot, Bnot = C, 0, X
+	}
+	r = uint8(math.Round((Rnot + m) * 255))
+	g = uint8(math.Round((Gnot + m) * 255))
+	b = uint8(math.Round((Bnot + m) * 255))
+	return r, g, b
 }
